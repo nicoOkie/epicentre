c39b193bbbe334704e8c05197a8fa6c9363afbb6
nicoOkie <chapon.nicola@gmail.com>
2019-08-21 16:00:40 +0200

    static nav component without styles

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -16,7 +16,52 @@ import './nav.scss';
  */
 const Nav = () => (
   <nav className="nav">
-   Ici navigation
+    <ul className="nav-list">
+      <li className="nav-list-users">
+        <h2>Utilisateurs</h2>
+        <ul>
+          <li>Licences et tarifs</li>
+          <li>Mises à jour</li>
+          <li>Vos suggestions</li>
+        </ul>
+      </li>
+      <li className="nav-list-discovery">
+        <h2>Découverte</h2>
+        <ul>
+          <li>Fiche descriptive</li>
+          <li>Saisie de la structure</li>
+          <li>Descente de charges</li>
+          <li>Calcul sismique</li>
+          <li>Vérification des murs</li>
+          <li>Eurocodes</li>
+          <li>Étude comparative</li>
+          <li>Bibliogarphie</li>
+          <li>Questions fréquentes</li>
+          <li>D'autres questions ?</li>
+          <li>Prêt d'évaluation</li>
+          <li>Offre éducation</li>
+          <li>Version gratuite</li>
+        </ul>
+      </li>
+      <li className="nav-list-docs">
+        <h2>Infos & doc</h2>
+        <ul>
+          <li>Accueil</li>
+          <li>Contact</li>
+          <li>Plan du site</li>
+          <li>Recommander ce site</li>
+        </ul>
+      </li>
+      <li className="nav-list-admin">
+        <ul>
+          <li>Accueil</li>
+          <li>Contact</li>
+          <li>Plan du site</li>
+          <li>Recommander ce site</li>
+        </ul>
+      </li>
+      <li className="nav-list-print">Imprimer</li>
+    </ul>
   </nav>
 );
 
